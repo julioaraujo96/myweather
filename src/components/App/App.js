4d7ed5fcdc6e81702338645b27023c3f9bde190f
julioaraujo96 <julioaraujo.bs@gmail.com>
2019-08-16 01:06:07 +0100

    helmet title

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Helmet } from "react-helmet";
 import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>My Weather</title>
+    </Helmet>
+    <h1>Hello World</h1>
+  </div>
+);
 
 export default App;
