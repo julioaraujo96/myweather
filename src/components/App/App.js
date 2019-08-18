dcb08a0d383a511eaf071f2cecfabdb83ccec465
julioaraujo96 <julioaraujo.bs@gmail.com>
2019-08-18 22:46:36 +0100

    setting up api call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 import "./App.module.scss";
 
-const App = () => (
-  <div className="App">
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>My Weather</title>
-    </Helmet>
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-    {/* <Titles /> */}
-    {/* <Search /> */}
-    {/* <Weather /> */}
-  </div>
-);
+class App extends Component {
+  getWeather = async () => {
+    const response = await fetch(
+      `http://openweathermap.org/data/2.5/forecast?q=London,us&appid=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>My Weather</title>
+        </Helmet>
+
+        {/* <Header /> */}
+        {/* <Search /> */}
+        {/* <Weather /> */}
+      </div>
+    );
+  }
+}
 
 export default App;
