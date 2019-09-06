034a12fc83fbc49f650ce623e3542cbb0d59f31b
julioaraujo96 <julioaraujo.bs@gmail.com>
2019-09-06 20:05:50 +0100

    Updates

---
+++ precache-manifest.6f8affefc6d0ad5d30100f3d6b4aacbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43204a3412dfa3c85ca8998cd7d0fee3",
+    "url": "/myweather/index.html"
+  },
+  {
+    "revision": "1c8a38528de959f172a6",
+    "url": "/myweather/static/css/main.923f449b.chunk.css"
+  },
+  {
+    "revision": "96f15470c7d854381d28",
+    "url": "/myweather/static/js/2.72cd397b.chunk.js"
+  },
+  {
+    "revision": "1c8a38528de959f172a6",
+    "url": "/myweather/static/js/main.a2922606.chunk.js"
+  },
+  {
+    "revision": "745884d2ac2fe654d860",
+    "url": "/myweather/static/js/runtime~main.f915440d.js"
+  }
+]);
