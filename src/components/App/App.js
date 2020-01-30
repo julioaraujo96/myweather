5c2720a03250c408a09e39fdd0c86b56535d4d63
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-01-30 00:15:07 +0000

    added loading spinner

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet';
 import styles from './App.module.scss';
 import LocationForm from '../LocationForm/LocationForm';
 import WeatherDisplay from '../WeatherDisplay/WeatherDisplay';
+import Loading from '../Loading/Loading';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = '//api.openweathermap.org/data/2.5/forecast';
@@ -10,7 +11,8 @@ const BASE_URL = '//api.openweathermap.org/data/2.5/forecast';
 class App extends Component {
   state = {
     weather: null,
-    error: false
+    error: false,
+    isLoading: false
   };
   getWeather = async (city, country) => {
     const response = await fetch(
@@ -27,12 +29,14 @@ class App extends Component {
 
   handleGetLocation = async ({ city, country }) => {
     try {
+      this.setState({ isLoading: true });
       const weatherData = await this.getWeather(city, country);
       const weather = await this.parseWeatherData(weatherData);
-      this.setState({ weather: weather, error: false });
+      this.setState({ weather: weather, error: false, isLoading: false });
     } catch ({ response, message }) {
       this.setState({
-        error: true
+        error: true,
+        isLoading: false
       });
     }
   };
@@ -51,30 +55,27 @@ class App extends Component {
       .slice(0, 5);
 
   render() {
+    let output = null;
     if (this.state.error) {
-      return (
-        <div className={styles.App}>
-          <Helmet>
-            <meta charSet='utf-8' />
-            <title>My Weather</title>
-          </Helmet>
-          {/* <Header /> */}
-          <LocationForm onSubmitLocation={this.handleGetLocation} />
-          <h3>Something went wrong... Make sure you enter a valid location.</h3>
-        </div>
+      output = (
+        <h3>Something went wrong... Make sure you enter a valid location.</h3>
+      );
+    } else if (this.state.weather) {
+      output = this.state.isLoading ? (
+        <Loading />
+      ) : (
+        <WeatherDisplay weatherData={this.state.weather} />
       );
     }
+
     return (
       <div className={styles.App}>
         <Helmet>
           <meta charSet='utf-8' />
           <title>My Weather</title>
         </Helmet>
-        {/* <Header /> */}
         <LocationForm onSubmitLocation={this.handleGetLocation} />
-        {this.state.weather && (
-          <WeatherDisplay weatherData={this.state.weather} />
-        )}
+        {output}
       </div>
     );
   }
---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Loading.scss';
+
+const Loading = () => (
+  <div className='sk-chase'>
+    <div className='sk-chase-dot'></div>
+    <div className='sk-chase-dot'></div>
+    <div className='sk-chase-dot'></div>
+    <div className='sk-chase-dot'></div>
+    <div className='sk-chase-dot'></div>
+    <div className='sk-chase-dot'></div>
+  </div>
+);
+
+export default Loading;
