ec73dbd1a49e91dc5a33d85dc19f3b00846feaae
julioaraujo96 <julioaraujo.bs@gmail.com>
2020-01-30 01:03:24 +0000

    fixed spinner position

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './Loading.scss';
 
 const Loading = () => (
-  <div className='sk-chase'>
-    <div className='sk-chase-dot'></div>
-    <div className='sk-chase-dot'></div>
-    <div className='sk-chase-dot'></div>
-    <div className='sk-chase-dot'></div>
-    <div className='sk-chase-dot'></div>
-    <div className='sk-chase-dot'></div>
+  <div className='loader'>
+    <div className='sk-chase'>
+      <div className='sk-chase-dot'></div>
+      <div className='sk-chase-dot'></div>
+      <div className='sk-chase-dot'></div>
+      <div className='sk-chase-dot'></div>
+      <div className='sk-chase-dot'></div>
+      <div className='sk-chase-dot'></div>
+    </div>
   </div>
 );
-
 export default Loading;
